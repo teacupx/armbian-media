cfdca10f30e9f72db9290bb9a8e0585b44b70141
JMCC <JMCC@forum.armbian.com>
2020-11-08 19:04:31 +0100

    Add libmali and re-organize directories

--- crea-packages.sh
+++ crea-packages.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-for i in media*
+#
+# Compiles all packages, and saves them to output/
+#
+mkdir -p output
+rm -rf output/*
+
+for i in metapackages/* libmali/*
 do
-
-VERSION=$( grep Version: $i/DEBIAN/control | cut -d' ' -f2 )
-ARCH=$( grep Architecture: $i/DEBIAN/control | cut -d' ' -f2 )
-PACKAGE_NAME=${i}_${VERSION}_${ARCH}.deb
-dpkg-deb -b $i $PACKAGE_NAME
-
+  PACKAGENAME=$( grep Package: $i/DEBIAN/control | cut -d' ' -f2 )
+  VERSION=$( grep Version: $i/DEBIAN/control | cut -d' ' -f2 )
+  ARCH=$( grep Architecture: $i/DEBIAN/control | cut -d' ' -f2 )
+  PACKAGE_NAME=${PACKAGENAME}_${VERSION}_${ARCH}.deb
+  dpkg-deb -b $i output/$PACKAGE_NAME
 done
+
+#for i in libmali/*
+#do
+#  PACKAGENAME=$( grep Package: $i/DEBIAN/control | cut -d' ' -f2 )
+#  VERSION=$( grep Version: $i/DEBIAN/control | cut -d' ' -f2 )
+#  ARCH=$( grep Architecture: $i/DEBIAN/control | cut -d' ' -f2 )
+#  PACKAGE_NAME=${PACKAGENAME}_${VERSION}_${ARCH}.deb
+#  dpkg-deb -b $i output/$PACKAGE_NAME
+#done
