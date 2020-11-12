35dda29b6458fc389c2ad91cf3cb94bc35e4e2f6
JMCC <JMCC@forum.armbian.com>
2020-11-12 01:44:47 +0000

    Add gl4es wrapper

---
+++ gl4es/arm64/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+rm -rf gl4es
+git clone https://github.com/ptitSeb/gl4es
+mkdir -p gl4es/build package/usr/lib/aarch64-linux-gnu ../../output
+cd gl4es/build
+cmake .. -DODROID=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDEFAULT_ES=2 -DGBM=1 -DCMAKE_C_FLAGS="-O2 -march=armv8-a -mtune=cortex-a53"
+make -j$(nproc --all)
+cd ../..
+cp gl4es/lib/libGL.so.1 package/usr/lib/aarch64-linux-gnu/
+dpkg-deb -b package ../../output/gl4es_1.1.4-0armbian1_arm64.deb
+rm -rf gl4es package/usr
---
+++ gl4es/armhf/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rf gl4es
+git clone https://github.com/ptitSeb/gl4es
+mkdir -p gl4es/build package/usr/lib/arm-linux-gnueabihf ../../output
+cd gl4es/build
+cmake .. -DODROID=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDEFAULT_ES=2 -DGBM=1 -DCMAKE_C_FLAGS="-O2 -march=armv7-a -mtune=cortex-a15.cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard"
+make -j$(nproc --all)
+cd ../..
+cp gl4es/lib/libGL.so.1 package/usr/lib/arm-linux-gnueabihf/
+dpkg-deb -b package ../../output/gl4es_1.1.4-0armbian1_armhf.deb
