f4ffe845f0ec45bdf7ab949f852d72b93341c93a
JMCC <JMCC@forum.armbian.com>
2020-12-22 20:05:21 +0100

    GL4ES: Version bump

--- gl4es/arm64/compile.sh
+++ gl4es/arm64/compile.sh
@@ -8,5 +8,5 @@ cmake .. -DODROID=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDEFAULT_ES=2 -DGBM=1 -DC
 make -j$(nproc --all)
 cd ../..
 cp gl4es/lib/libGL.so.1 package/usr/lib/aarch64-linux-gnu/
-dpkg-deb -b package ../../output/gl4es_1.1.4-0armbian1_arm64.deb
+dpkg-deb -b package ../../output/gl4es_1.1.5-0armbian1_arm64.deb
 rm -rf gl4es package/usr
