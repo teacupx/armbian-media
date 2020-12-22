8095217ca9ef03151f0447a3cd849f751f57a34e
JMCC <JMCC@forum.armbian.com>
2020-12-23 00:07:30 +0100

    Change gl4es lib name and use symlink

--- gl4es/arm64/compile.sh
+++ gl4es/arm64/compile.sh
@@ -7,6 +7,10 @@ cd gl4es/build
 cmake .. -DODROID=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDEFAULT_ES=2 -DGBM=1 -DCMAKE_C_FLAGS="-O2 -march=armv8-a -mtune=cortex-a53"
 make -j$(nproc --all)
 cd ../..
-cp gl4es/lib/libGL.so.1 package/usr/lib/aarch64-linux-gnu/
+cp gl4es/lib/libGL.so.1 package/usr/lib/aarch64-linux-gnu/libGL.so.99
+cd package/usr/lib/aarch64-linux-gnu
+ln -s libGL.so.99 libGL.so.1
+cd ../../../..
 dpkg-deb -b package ../../output/gl4es_1.1.5-0armbian1_arm64.deb
 rm -rf gl4es package/usr
+
--- gl4es/armhf/compile.sh
+++ gl4es/armhf/compile.sh
@@ -6,5 +6,10 @@ cd gl4es/build
 cmake .. -DODROID=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDEFAULT_ES=2 -DGBM=1 -DCMAKE_C_FLAGS="-O2 -march=armv7-a -mtune=cortex-a15.cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard"
 make -j$(nproc --all)
 cd ../..
-cp gl4es/lib/libGL.so.1 package/usr/lib/arm-linux-gnueabihf/
+cp gl4es/lib/libGL.so.1 package/usr/lib/arm-linux-gnueabihf/libGL.so.99
+cd package/usr/lib/arm-linux-gnueabihf
+ln -s libGL.so.99 libGL.so.1
+cd ../../../..
 dpkg-deb -b package ../../output/gl4es_1.1.5-0armbian1_armhf.deb
+rm -rf gl4es package/usr
+
